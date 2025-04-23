7e211eaeb085460e819874fa53500785703128d5
diogofragoso <diogofragoso@yahoo.com.br>
2025-04-22 21:47:47 -0300

    modifiquei a porta do vite para 8080

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+// trecho adicionado para lab 21
+server: {
+  host: '127.0.0.1',
+  port: 8080 // você pode mudar a porta se quiser
+}
+// fim do trecho
+
+
+
+
 })
