4ca04c812e221c45ef66730f6dc4067d4df9cdfe
diogofragoso <diogofragoso@yahoo.com.br>
2025-05-27 21:42:32 -0300

    NavPainel + PainelDashBoard

---
+++ frontend/src/pages/PainelDashBoard/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import NavPainel from '../../components/NavPainel';
+
+function PainelDashBoard() {
+  const navegacao = {
+    linkNome1: 'Cursos',
+    linkNome2: 'Estudantes',
+    linkNome3: 'Turmas',
+    link1: '/PainelCurso/EditarCurso',
+    link2: '/PainelEstudante/RegistroEstudante',
+    link3: '/PainelTurma/ListarTurma',
+  };
+
+  return (
+    <div>
+      <Container fluid className='px-0 mt-0'>
+        <div className='row'>
+          <NavPainel navegacao={navegacao} />
+        </div>
+      </Container>
+      <Outlet />
+    </div>
+  );
+}
+
+export default PainelDashBoard;
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -18,6 +18,8 @@ import InserirEstudanteCurso from "./components/InserirEstudanteCurso";
 import Login from "./components/Login";
 import PainelTurma from "./pages/PainelTurma";
 import ListarTurmas from "./components/ListarTurma";
+import NavPainel from "./components/NavPainel";
+import PainelDashBoard from "../src/pages/PainelDashBoard";
 
 
 function AppRoutes() {
@@ -33,36 +35,40 @@ function AppRoutes() {
             {/* Rota aninhada dentro de PainelEstudante */}
             <Route path="RegistroEstudante" element={<RegistroEstudante />} />
           </Route>
-          
+
           <Route path="PainelCurso" element={<PainelCurso />}>
             {/* Rota aninhada dentro de PainelCurso */}
-            <Route path="RegistroCurso" element={<RegistroCurso />} />  
+            <Route path="RegistroCurso" element={<RegistroCurso />} />
             <Route path="EditarCurso" element={<EditarCurso />} />
           </Route>
 
           <Route path="PainelUc" element={<PainelUc />}>
             {/* Rota aninhada dentro de PainelCurso */}
-            <Route path="RegistroUc" element={<RegistroUc />} />  
-            <Route path="InserirEstudanteCurso" element={<InserirEstudanteCurso />} />  
+            <Route path="RegistroUc" element={<RegistroUc />} />
+            <Route path="InserirEstudanteCurso" element={<InserirEstudanteCurso />} />
           </Route>
 
-          <Route path="PainelIndicadores" element={<PainelIndicadores />}>          
-           <Route path="ListarIndicadores" element={<ListarIndicadores />} />  
+          <Route path="PainelIndicadores" element={<PainelIndicadores />}>
+            <Route path="ListarIndicadores" element={<ListarIndicadores />} />
           </Route>
 
-          <Route path="PainelAvaliativa" element={<PainelAvaliativa />}>          
-           <Route path="ListarAvaliativa" element={<ListarAvaliativa />} />  
+          <Route path="PainelAvaliativa" element={<PainelAvaliativa />}>
+            <Route path="ListarAvaliativa" element={<ListarAvaliativa />} />
           </Route>
-            <Route path="PainelTurma" element={<PainelTurma />}>
+          <Route path="PainelTurma" element={<PainelTurma />}>
             {/* Rota aninhada dentro de PainelTurma */}
             <Route path="ListarTurma" element={<ListarTurmas />} />
-            </Route>       
-              
+          </Route>
 
-          
-          
+          <Route path="PainelDashBoard" element={<PainelDashBoard />} >
+          <Route path="NavPainel" element={<NavPainel />} />
+          </Route>
+
+
+
+
         </Route>
-          <Route path="/Login" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
 
         {/* Página Não Encontrada */}
         {/* <Route path="*" element={<PageNotFound />} /> */}
