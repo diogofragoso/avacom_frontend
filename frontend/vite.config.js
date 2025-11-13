378a83452d23e9fd1375bd2daf0a03eeceacec81
diogofragoso <diogofragoso@yahoo.com.br>
2025-11-13 20:27:22 -0300

    bug: testanto linha base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   
-  base: "/avacom_frontend/", // <--- VERIFIQUE ESTA LINHA COM MUITA ATENÇÃO
+  // base: "/avacom_frontend/", 
+  base: "/", 
 
   server: {
     host: '127.0.0.1',
