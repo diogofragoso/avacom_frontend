964ed227091d9bcd584950efb84d3d50986a0b56
diogofragoso <diogofragoso@yahoo.com.br>
2025-11-15 12:40:49 -0300

    bug: Ajuste de rotas e redirecionamento após o login ok

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,4 +1,4 @@
-import {HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Board from "./pages/Board";
 import Tarefas from "./pages/Tarefas";
@@ -19,67 +19,66 @@ import Login from "./components/Login";
 import PainelTurma from "./pages/PainelTurma";
 import ListarTurmas from "./components/ListarTurma";
 import NavPainel from "./components/NavPainel";
-import PainelDashBoard from "../src/pages/PainelDashBoard";
+import PainelDashBoard from "./pages/PainelDashBoard";
 import GerenciarTurma from "./components/GerenciarTurma";
 import AvaliacaoEstudante from "./components/AvaliacaoEstudante";
 import GerenciarAvaliativa from "./components/GerenciarAvaliativas";
 import MetricasTurma from "./components/MetricasTurmas";
 
-
 function AppRoutes() {
   return (
-    // <BrowserRouter>
     <HashRouter>
       <Routes>
-        {/* Rota principal com rotas aninhadas */}
-        <Route path="/" element={<Login />}>
-          <Route path="Board" element={<Board />} />
-          <Route path="Tarefas" element={<Tarefas />} />
-          <Route path="Contador" element={<Contador />} />
-          <Route path="PainelEstudante" element={<PainelEstudante />}>
-            <Route path="RegistroEstudante" element={<RegistroEstudante />} />
-          </Route>
 
-          <Route path="PainelCurso" element={<PainelCurso />}>
-            <Route path="RegistroCurso" element={<RegistroCurso />} />
-            <Route path="EditarCurso" element={<EditarCurso />} />
-          </Route>
+        {/* Login sozinho */}
+        <Route path="/" element={<Login />} />
 
-          <Route path="PainelUc" element={<PainelUc />}>
-            <Route path="RegistroUc" element={<RegistroUc />} />
-            <Route path="InserirEstudanteCurso" element={<InserirEstudanteCurso />} />
-          </Route>
+        {/* Home no nível raiz */}
+        <Route path="/Home" element={<Home />} />
 
-          <Route path="PainelIndicadores" element={<PainelIndicadores />}>
-            <Route path="ListarIndicadores" element={<ListarIndicadores />} />
-          </Route>
+        {/* Outras rotas no nível raiz */}
+        <Route path="Board" element={<Board />} />
+        <Route path="Tarefas" element={<Tarefas />} />
+        <Route path="Contador" element={<Contador />} />
 
-          <Route path="PainelAvaliativa" element={<PainelAvaliativa />}>
-            <Route path="ListarAvaliativa" element={<ListarAvaliativa />} />
-          </Route>
+        <Route path="PainelEstudante" element={<PainelEstudante />}>
+          <Route path="RegistroEstudante" element={<RegistroEstudante />} />
+        </Route>
 
-          <Route path="PainelTurma" element={<PainelTurma />}>
-            <Route path="ListarTurma" element={<ListarTurmas />} />
+        <Route path="PainelCurso" element={<PainelCurso />}>
+          <Route path="RegistroCurso" element={<RegistroCurso />} />
+          <Route path="EditarCurso" element={<EditarCurso />} />
+        </Route>
 
-            {/* GerenciarTurma e GerenciarAvaliativa no mesmo nível */}
-            <Route path="GerenciarTurma" element={<GerenciarTurma />} />
-            <Route path="GerenciarAvaliativa" element={<GerenciarAvaliativa />} />
-            <Route path="MetricasTurmas" element={<MetricasTurma />} />
-            <Route path="AvaliacaoEstudante" element={<AvaliacaoEstudante />} />
-          </Route>
+        <Route path="PainelUc" element={<PainelUc />}>
+          <Route path="RegistroUc" element={<RegistroUc />} />
+          <Route path="InserirEstudanteCurso" element={<InserirEstudanteCurso />} />
+        </Route>
 
-          <Route path="PainelDashBoard" element={<PainelDashBoard />} >
-            <Route path="NavPainel" element={<NavPainel />} />
-            
-          </Route>
+        <Route path="PainelIndicadores" element={<PainelIndicadores />}>
+          <Route path="ListarIndicadores" element={<ListarIndicadores />} />
+        </Route>
+
+        <Route path="PainelAvaliativa" element={<PainelAvaliativa />}>
+          <Route path="ListarAvaliativa" element={<ListarAvaliativa />} />
+        </Route>
+
+        <Route path="PainelTurma" element={<PainelTurma />}>
+          <Route path="ListarTurma" element={<ListarTurmas />} />
+          <Route path="GerenciarTurma" element={<GerenciarTurma />} />
+          <Route path="GerenciarAvaliativa" element={<GerenciarAvaliativa />} />
+          <Route path="MetricasTurmas" element={<MetricasTurma />} />
+          <Route path="AvaliacaoEstudante" element={<AvaliacaoEstudante />} />
+        </Route>
+
+        <Route path="PainelDashBoard" element={<PainelDashBoard />} >
+          <Route path="NavPainel" element={<NavPainel />} />
         </Route>
 
         <Route path="/Login" element={<Login />} />
 
-        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
-      </HashRouter>
-    /* </BrowserRouter> */
+    </HashRouter>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
 
   server: {
     host: '127.0.0.1',
-    port: 8080 
+    port: 8081
   }
 })
